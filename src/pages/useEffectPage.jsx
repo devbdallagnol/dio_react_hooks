b6339b41751329c4d28497f8dd36c37c6aa2f0db
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 10:52:57 -0300

    update - useEffectPage.jsx - criando input Name e Senha

--- src/pages/useEffectPage.jsx
+++ src/pages/useEffectPage.jsx
@@ -1,9 +1,23 @@
-import react from "react";
+import react, { useState } from "react";
 
 export const UseEffectPage = () => {
-    return (
-        <h2>Bem vindo ao UseEffectPage</h2>
-    )
-}
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <>
+      <h2>Bem vindo ao UseEffectPage</h2>
+      <input
+        placeholder="Nome"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        placeholder="Senha"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+    </>
+  );
+};
 
 export default UseEffectPage;
