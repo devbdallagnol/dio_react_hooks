366368120b134ef1f86adb33613b790e35926962
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 11:13:08 -0300

    update - useCallback.jsx - importando hook useCallback

--- src/pages/useCallBack.jsx
+++ src/pages/useCallBack.jsx
@@ -1,21 +1,20 @@
-import React, {useState} from "react";
+import React, { useState, useCallback } from "react";
 
 export const UseCallbackPage = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
-  const onChangeName = (event) => {
-    setName(event.target.value)
-  }
+  //   const onChangeName = (event) => {
+  //     setName(event.target.value)
+  //   }
+  const onChangeName = useCallback((event) => {
+    setName(event.target.value);
+  }, []);
 
   return (
     <>
       <h2>Bem vindo ao UseCallbackPage</h2>
-      <input
-        placeholder="Nome"
-        value={name}
-        onChange={onChangeName}
-      />
+      <input placeholder="Nome" value={name} onChange={onChangeName} />
       <input
         placeholder="Senha"
         value={password}
