8fd5ddc01ddc1934e139fd9acdd64643e8560dec
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 12:19:26 -0300

    update - homePage.jsx - styled-components para botões

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,17 +1,44 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import {UserContext} from '../contexts/useContext';
+import { UserContext } from "../contexts/useContext";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  margin: 10px 10px;
+  padding: 15px;
+  color: #fff;
+  background-color: #007bff;
+  text-decoration: none;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  border: 1px solid #00ff00;
+  border-radius: 50px;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:active {
+    background-color: #003f7f;
+  }
+`;
+
+const Title = styled.h1`
+  color: #333;
+`;
 
 export const HomePage = () => {
   const user = useContext(UserContext);
   return (
     <>
-      <h1>Bem vindo aos hooks, {user.userName}!</h1>
-      <Link to="/useState">Ir para o Site UseStatePage</Link><br/><br/>
-      <Link to="/useEffect">Ir para o Site UseEffectPage</Link><br/><br/>
-      <Link to="/useCallback">Ir para o Site UseCallbackPage</Link><br/><br/>
-      <Link to="/useMemo">Ir para o Site UseMemoPage</Link><br/><br/>
-      <Link to="/useRef">Ir para o Site UseRefPage</Link><br/><br/>
+      <Title>Bem vindo aos hooks, {user.userName}!</Title>
+      <hr/>
+      <StyledLink to="/useState">Ir para o Site UseStatePage</StyledLink><br/>
+      <StyledLink to="/useEffect">Ir para o Site UseEffectPage</StyledLink><br/>
+      <StyledLink to="/useCallback">Ir para o Site UseCallbackPage</StyledLink><br/>
+      <StyledLink to="/useMemo">Ir para o Site UseMemoPage</StyledLink><br/>
+      <StyledLink to="/useRef">Ir para o Site UseRefPage</StyledLink>
     </>
   );
 };
