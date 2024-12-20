169ced7782d1e811d8b51ea19e0db94b5e544b2e
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 11:33:54 -0300

    update - useMemoPage.jsx - useMemo - memorizando o Validate Password

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -7,7 +7,8 @@ export const HomePage = () => {
       <h1>Bem vindo aos hooks!</h1>
       <Link to="/useState">Ir para o Site UseStatePage</Link><br/><br/>
       <Link to="/useEffect">Ir para o Site UseEffectPage</Link><br/><br/>
-      <Link to="/useCallback">Ir para o Site UseCallback</Link><br/><br/>
+      <Link to="/useCallback">Ir para o Site UseCallbackPage</Link><br/><br/>
+      <Link to="/useMemo">Ir para o Site UseMemoPage</Link><br/><br/>
     </>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {HomePage} from './pages/homePage';
 import {UseStatePage} from './pages/useStatePage';
 import {UseEffectPage} from './pages/useEffectPage';
 import {UseCallbackPage} from './pages/useCallBack';
+import {UseMemoPage} from './pages/useMemoPage';
 
 export const AppRoutes = () => {
     return (
@@ -12,6 +13,7 @@ export const AppRoutes = () => {
                 <Route path='/useState' element={<UseStatePage />}/>
                 <Route path='/useEffect' element={<UseEffectPage />}/>
                 <Route path='/useCallback' element={<UseCallbackPage />}/>
+                <Route path='/useMemo' element={<UseMemoPage />}/>
             </Routes>
         </BrowserRouter>
     )
