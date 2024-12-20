9ab0181891b8421011efa4f72e0fcf4c147f4918
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 10:28:23 -0300

    update - routes.js - Bem vindo ao useState Page

---
+++ src/pages/useStatePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const UseStatePage = () => {
+    return (
+        <h2> Bem vindo ao UseStatePage</h2>
+    )
+}
+
+export default UseStatePage;
+
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {HomePage} from './pages/homePage';
+import {UseStatePage} from './pages/useStatePage';
 
 export const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/home' element={<HomePage />}/>
+                <Route path='/useState' element={<UseStatePage />}/>
             </Routes>
         </BrowserRouter>
     )
