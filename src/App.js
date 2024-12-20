80b2f4ed69fe65f8766a450de813ab80c6008e68
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 10:16:28 -0300

    update - estrutura inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomePage from "./pages/homePage";
+
+function App() {
+  return (
+    <div className="App">
+      <h5>Hello World!</h5>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const HomePage = () => {
+    return (<h1>Bem vindo aos hooks!</h1>)
+}
+
+export default HomePage;
