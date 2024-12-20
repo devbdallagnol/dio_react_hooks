d8ecc24597395de3bafa0d6bf826737f0d18b470
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 10:36:20 -0300

    update - useStatePage.jsx - import useState - count

--- src/pages/useStatePage.jsx
+++ src/pages/useStatePage.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const UseStatePage = () => {
-    return (
-        <h2> Bem vindo ao UseStatePage</h2>
-    )
-}
+  const [count, setcount] = useState(0);
+  return (
+    <>
+      <h2> Bem vindo ao UseStatePage</h2>
+      <p>{count}</p>
+    </>
+  );
+};
 
 export default UseStatePage;
-
