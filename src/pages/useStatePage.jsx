6bbf72e9953d1bc96def13fd1232db0a597fa8ef
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 10:40:32 -0300

    update - useStatePage.jsx - useState - button counter

--- src/pages/useStatePage.jsx
+++ src/pages/useStatePage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
 export const UseStatePage = () => {
-  const [count, setcount] = useState(0);
+  const [count, setCount] = useState(0);
   return (
     <>
       <h2> Bem vindo ao UseStatePage</h2>
       <p>{count}</p>
+      <button onClick={() => setCount(count+1)}>Adicionar</button>
+      <button onClick={() => setCount(count-1)}>Diminuir</button>
     </>
   );
 };
