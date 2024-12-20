6ab3982a266a8eed2c4cc5b6339fe107252bf897
Bruno DallAgnol <bruno.techdatascientist@gmail.com>
2024-12-20 11:57:16 -0300

    update - useContext - passando elemento de pai para filho

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Link } from "react-router-dom";
+import {UserContext} from '../contexts/useContext';
 
 export const HomePage = () => {
+  const user = useContext(UserContext);
   return (
     <>
-      <h1>Bem vindo aos hooks!</h1>
+      <h1>Bem vindo aos hooks, {user.userName}!</h1>
       <Link to="/useState">Ir para o Site UseStatePage</Link><br/><br/>
       <Link to="/useEffect">Ir para o Site UseEffectPage</Link><br/><br/>
       <Link to="/useCallback">Ir para o Site UseCallbackPage</Link><br/><br/>
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,25 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {HomePage} from './pages/homePage';
-import {UseStatePage} from './pages/useStatePage';
-import {UseEffectPage} from './pages/useEffectPage';
-import {UseCallbackPage} from './pages/useCallBack';
-import {UseMemoPage} from './pages/useMemoPage';
-import {UseRefPage} from './pages/useRefPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/homePage";
+import { UseStatePage } from "./pages/useStatePage";
+import { UseEffectPage } from "./pages/useEffectPage";
+import { UseCallbackPage } from "./pages/useCallBack";
+import { UseMemoPage } from "./pages/useMemoPage";
+import { UseRefPage } from "./pages/useRefPage";
+import { UserContextProvider } from "./contexts/useContext";
 
 export const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/home' element={<HomePage />}/>
-                <Route path='/useState' element={<UseStatePage />}/>
-                <Route path='/useEffect' element={<UseEffectPage />}/>
-                <Route path='/useCallback' element={<UseCallbackPage />}/>
-                <Route path='/useMemo' element={<UseMemoPage />}/>
-                <Route path='/useRef' element={<UseRefPage />}/>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/useState" element={<UseStatePage />} />
+          <Route path="/useEffect" element={<UseEffectPage />} />
+          <Route path="/useCallback" element={<UseCallbackPage />} />
+          <Route path="/useMemo" element={<UseMemoPage />} />
+          <Route path="/useRef" element={<UseRefPage />} />
+        </Routes>
+      </UserContextProvider>
+    </BrowserRouter>
+  );
+};
